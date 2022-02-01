e0597131de4e058e6f64580b9877e56cfb3e6626
Alex Wolf <f.alexander.wolf@gmail.com>
2022-02-01 18:35:02 +0100

    reveal decks

---
+++ talks/2016-02-04-icb-lunch/figs/cp_fig.sh
@@ -0,0 +1,4 @@
+cp ../../../../../01_projects/1510_grnpseudotime/code/figs/toggleswitch_trajectory_cell001.svg .
+cp ../../../../../01_projects/1510_grnpseudotime/code/figs/illustrate_1.svg .
+cp ../../../../../01_projects/1510_grnpseudotime/code/figs/illustrate_2.svg .
+
